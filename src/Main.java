a8ef1eaaf5c8293bb34a1b54b06f302bba71b6b1
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-05-01 18:15:29 -0600

    Dos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        LeerArchivo lectura = new LeerArchivo();
+        try {
+            lectura.metodoUno("datos.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
